67070496bcf2a4d3595098c7f6d8b9e64cdb6ff4
Matt Levine <mattlevine22@gmail.com>
2019-11-01 09:25:32 -0700

    adding random lorenz init function

---
+++ get_lorenz_inits.m
@@ -0,0 +1,23 @@
+function init_vec = get_lorenz_inits(n)
+%% uniform box random sampling for loren initial conditions not too far from the attractor
+xmin = -10;
+xmax = 10;
+ymin = -20;
+ymax = 30;
+zmin = 10;
+zmax = 40;
+
+if nargin < 1
+    n = 1;
+end
+init_vec = zeros(n, 3);
+for k=1:n
+    xrand = xmin+(xmax-xmin)*rand;
+    yrand = ymin+(ymax-ymin)*rand;
+    zrand = zmin+(zmax-zmin)*rand;
+    init_vec(k,:) = [xrand, yrand, zrand];
+end
+
+end
+
+
