a283a4075e506675587cb709b137e2411253cd0f
Matt Levine <mattlevine22@gmail.com>
2019-11-07 03:44:41 -0500

    removing unneeded files

--- partial_derivative.m
+++
@@ -1,6 +0,0 @@
-function y=partial_derivative( state_pred_before, meas_now, K, true_traj_now)
-psivalue=Psi(state_pred_before);
-a=meas_now-H*psivalue;
-b=psivalue-true_traj_now;
-y=2*(K*a+b)*a';
-end
